d3d0c81c02ee2ce5f0631f464e1bfb7ba5dad65a
0wliver <theowlet@126.com>
2018-10-26 12:42:40 +0800

    add xgboost&lightgbm demo+intro

---
+++ Machine Learning/Regression/XGboost_intro.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+# =============================================================================
+# 安装
+
+# conda install -c conda-forge py-xgboost
+
+# =============================================================================
+
+from xgboost.sklearn import XGBClassifier
+from xgboost.sklearn import XGBRegressor
+
+# =============================================================================
+# XGboost
+
+# 介绍
+# https://blog.csdn.net/v_JULY_v/article/details/81410574
+# https://www.jianshu.com/p/7467e616f227
+
+# 原生与sklearn 对比
+# https://blog.csdn.net/PIPIXIU/article/details/80463565
+
+# 调参
+# https://blog.csdn.net/ruding/article/details/78328835
+# https://www.cnblogs.com/wanglei5205/p/8579244.html
+
+# 文档
+# https://xgboost.readthedocs.io/en/latest/parameter.html
+# https://xgboost.readthedocs.io/en/latest/python/python_api.html#module-xgboost.sklearn
+# https://blog.csdn.net/lujiandong1/article/details/52777168
+
+# =============================================================================
